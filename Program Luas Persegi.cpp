53facb2f03de72e9ed08f722ae1bdef2e4916943
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-03 10:08:18 +0700

    Program C++

---
+++ Program Luas Persegi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float s, luas;
+	
+	cout<<"======================================="<<endl;
+	cout<<"====Program Menghitung Luas Persegi===="<<endl;
+	cout<<"======================================="<<endl;
+	
+	cout<<"Masukkan Panjang Sisi Persegi : ";
+	cin>>s;
+	
+	luas=s*s;
+	
+	cout<<"Luas Persegi adalah "<<luas;
+}
