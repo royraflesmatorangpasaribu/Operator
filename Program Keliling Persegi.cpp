c66022bcf6e98ff896d134a30df3a618ea552268
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-03 10:31:22 +0700

    Program C++

---
+++ Program Keliling Persegi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	float s, keliling;
+	
+	cout<<"======================================="<<endl;
+	cout<<"==Program Menghitung Keliling Persegi=="<<endl;
+	cout<<"======================================="<<endl;
+	
+	cout<<"Masukkan Panjang Sisi Persegi : ";
+	cin>>s;
+	
+	keliling=4*s;
+	
+	cout<<"Keliling Persegi adalah "<<keliling;
+}
