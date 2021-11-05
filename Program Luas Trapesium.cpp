b5266b20bee2545e786a26aea924b501f183d482
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-05 16:50:23 +0700

    Program C++

---
+++ Program Luas Trapesium.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	float s1, s2, t, luas;
+	
+	cout<<"====================================="<<endl;
+	cout<<"= Program Menghitung Luas Trapesium ="<<endl;
+	cout<<"====================================="<<endl;
+	
+	cout<<"Masukkan Panjang Sisi 1 Trapesium : ";
+	cin>>s1;
+	cout<<"Masukkan Panjang Sisi 2 Trapesium : ";
+	cin>>s2;
+	cout<<"Masukkan Tinggi Trapesium : ";
+	cin>>t;
+	
+	luas=((s1+s2)*t)/2;
+	
+	cout<<"\nLuas Trapesium adalah : "<<luas<<endl;
+	
+	return 0;
+}
