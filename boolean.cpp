5a58162e63efe2472578a8dfe4506e8ed9c7cdd1
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2021-11-15 17:25:43 +0700

    Program C++

---
+++ boolean.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int x = 10;
+  int y = 9;
+  cout << (x > y)<<endl;; // mengembalikan 1 (true), karena 10 lebih tinggi dari 9
+  cout << (x < y); // mengembalikan 0 (false), karena 10 lebih tinggi dari 9
+  return 0;
+}
