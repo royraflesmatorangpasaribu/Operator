ba3a9adfd7de4165cbb2fde1ee34bc8ccd047233
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2022-01-02 19:51:01 +0700

    Add files via upload

---
+++ umur.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int tahunLahir, usia;
+	
+	cout<<"Program Menentukan Usia"<<endl;
+	cout<<"oleh royrafmp"<<endl<<endl;
+	
+	cout<<"Masukkan Tahun Lahir Anda : ";
+	cin>>tahunLahir;
+	
+	usia=2022-tahunLahir;
+	
+	cout<<"Usia Anda Saat ini adalah "<<usia<<" Tahun";
+}
